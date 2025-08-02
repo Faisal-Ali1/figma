6040a27025cb5fbe4e50b3d57445707105077e1a
Faisal Ali <fali77616@gmail.com>
2025-08-02 16:04:44 +0530

    add featured Categories

--- frontned/src/Components/featuredCategories.jsx
+++ frontned/src/Components/featuredCategories.jsx
@@ -3,15 +3,17 @@ import featuredImg from "../utils/featuredImg";
 function FeaturedCategories(){
     return(
         <>
-        <div>
-            <h2>Featured Categories</h2>
-            <p>Get some inspiration 86k+ skills</p>
+        <div className="text-center mt-30">
+            <h2 className="text-3xl font-semibold mb-2">Featured Categories</h2>
+            <p className="text-sm">Get some inspiration 86k+ skills</p>
 
-            <div className="border h-20">
+            <div className=" flex justify-around mt-20">
                 {
                     featuredImg?.map((item) => (
-                        <div>
-                            <img src={`Images/${item}`} alt="" />
+                        <div className=" p-3 rounded-4xl bg-[#ECECEC] text-center">
+                            <img src={`Images/${item?.img}`} alt="" className=" h-70 w-82 bg-white rounded-4xl"/>
+                            <h2 className="font-semibold text-xl mt-2 mb-1">{item?.heading}</h2>
+                            <p>{item?.text}</p>
                         </div>
                     ))
                 }
--- frontned/src/utils/featuredImg.js
+++ frontned/src/utils/featuredImg.js
@@ -1,3 +1,15 @@
- const featuredImg = [ 'man-marketing-on-digital-.svg' , '1-header-min-7-scaled 1.svg' , 'digital-marketing.svg'];
+ const featuredImg = [ {
+    img: 'man-marketing-on-digital-.svg',
+    heading: "Sales Marketing",
+    text: "850 Skills"
+ } ,{
+    img:  '1-header-min-7-scaled 1.svg',
+    heading: "Sales Marketing",
+    text: "850 Skills",
+ } , {
+    img: 'digital-marketing.svg',
+    heading: "Sales Marketing",
+    text: "850 Skills"
+ }];
 
 export default featuredImg;
